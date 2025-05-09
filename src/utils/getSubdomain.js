cca86692aa01d265db1ff76d399894e6ff7f7fa7
X. Samuel Ji <samuel.j032@icloud.com>
2025-05-08 23:36:54 -0700

    fix: update getSubdomain function to correctly handle subdomains for vercel.app

--- src/utils/getSubdomain.js
+++ src/utils/getSubdomain.js
@@ -4,6 +4,9 @@ export const getSubdomain = () => {
   if (host.endsWith("localhost")) {
     return parts.slice(0, -1).join(".");
   }
+  if (host.endsWith("vercel.app")) {
+    return parts.slice(0, -3).join(".");
+  }
   if (parts.length <= 2) {
     return "";
   }
